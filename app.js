a2e66aeb5d66e0aac11288b2f3cca3cb8c0452de
Maria <mariapdevani@gmail.com>
2018-02-14 13:20:24 -0800

    mp

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const readLine = require('readline-sync');
 
 
 let database = [];
+let cart;
 
-function runLogin() {
+//let products = new ProductList();
+
+function runApp() {
   while(true) {
   var accountQuestion = readLine.question(' Enter (1) If you would like to create an account. (2) if you would like to login. (3) To exit.')
     if(accountQuestion === '1') {
     	createAccount()
+    	let productList = new ProductList();
+    	productList.populateWithDummyData();
+		productList.browseProducts();
+		cart.checkOut();
     } else if (accountQuestion === '2') {
     	login()
     } else if (accountQuestion === '3') {
@@ -32,9 +39,7 @@ function createAccount() {
 	database.push(new user(usernameInput, passwordInput))
 	if(usernameInput !== null && passwordInput !== null){
 		console.log("Your account has been created");
-		let productList = new ProductList();
-		productList.populateWithDummyData();
-		productList.browseProducts();
+		
 	}
 }
 	
@@ -59,7 +64,7 @@ class ProductList {
 	}
 
 	addProduct(product) {
-		this.products[product.ID] = product;
+		this.products[product.ID] = product; //I don't get this part
 	}
 
 	populateWithDummyData() {
@@ -78,28 +83,59 @@ class ProductList {
 		for (let i in this.products) {
 			let product = this.products[i];
 			//console.log(`(${product.ID})  ${product.name} : ${product.description} : ${product.price} `);
-			console.log("(" + product.ID + ")", product.name, ":", product.description, ":", product.price);
+			console.log("(" + product.ID + ")", product.name, ":", product.description, ":", product.price, ":");
 		}
 		const choice = readLine.question("Choose one: ");
+		let p = this.products[choice]; //need this explained better
+		cart.addItem(p); //need this explained better
+		cart.toString(); //need this explained better
 	}
 
 
 	toString() {
-    for (let i in this.products) {
-      let product = this.products[i];
-      console.log(product.name, ": ", product.description);
-    }
+	    for (let i in this.products) {
+	      let product = this.products[i];
+	      console.log(product.name, ": ", product.description);
+	    }
 	}
 }
 
 class Product {
-	constructor(id, name, description, price) {
+	constructor(id, name, description, price, quantity) {
 		this.ID = id;
 		this.name = name;
 		this.description = description;
 		this.price = price || 0;
+		this.quantity = quantity || 0;
 	}
 }
 
-runLogin();
 
+class ShoppingCart {
+	constructor() {
+		this.items = {};
+	}
+
+	addItem(product) {
+		this.items[product.ID] = product  //I don't get this part
+	}
+
+
+	toString() {
+		console.log(this.items);
+	}
+
+	checkOut() {
+		let numItems = readLine.question('how many would you like?')
+		for(let i in this.items){
+			let newItem = this.items[i]
+			console.log(this.items[i].price * Number(numItems))
+		}
+	}
+
+
+}
+
+cart = new ShoppingCart();
+
+runApp();
